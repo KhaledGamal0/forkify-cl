ad0f4b043f82e37caac51120e9678bea073cdbd0
Khaled Gamal <khaledgamaldevelpoer@example.com>
2024-02-06 21:56:02 +0200

    Your commit message

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd90cb222-0e38-4797-91fe-32221f10a764';
+export const MODAL_CLOSE_SEC = 2.5;
